a7f71d9ba8fbc7b571ae7d2bdba584012e3dc394
tori hill <130721235+bigsaggypotato@users.noreply.github.com>
2024-10-13 19:00:19 +1100

    Update main.tf
    Update main.tf to deploy two EC2 instances for the app and one for the PostgreSQL database.
    Added ELB to distribute traffic between the app instances.

--- A2_SupportingFiles/main.tf
+++ A2_SupportingFiles/main.tf
@@ -79,3 +79,29 @@ output "instance_public_ip" {
   value = aws_instance.foo_app_instance.public_ip
   description = "The public IP of the Foo App EC2 instance"
 }
+
+# Add Load Balancer and Resiliency
+resource "aws_lb" "app_lb" {
+  name               = "app-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_secg.id]
+  subnets            = aws_subnet.app_subnets[*].id
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
