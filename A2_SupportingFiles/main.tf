15d8f30fe294eef196719c1cd5ea28ece1c48b17
tori hill <130721235+bigsaggypotato@users.noreply.github.com>
2024-10-13 18:16:54 +1100

    Update main.tf

--- A2_SupportingFiles/main.tf
+++ A2_SupportingFiles/main.tf
@@ -1,12 +1,17 @@
-provider "aws" {
-  region = "us-east-1" 
-}
+terraform {
 
-resource "aws_instance" "foo_app_instance" {
-  ami           = "ami-0c55b159cbfafe1f0" 
-  instance_type = "t2.micro"      # Smallest instance for testing
-  tags = {
-    Name = "FooApp-Instance"
+  required_providers {
+
+    aws = {
+
+      source  = "hashicorp/aws"
+
+      version = "~> 4.0"
+
+    }
   }
 }
 
+provider "aws" {
+  region = "us-east-1"  
+}
