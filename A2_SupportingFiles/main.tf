691c2379acf8b4b93d2672485eb9485dd914430f
tori hill <130721235+bigsaggypotato@users.noreply.github.com>
2024-10-13 16:59:17 +1100

    Update main.tf

--- A2_SupportingFiles/main.tf
+++ A2_SupportingFiles/main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region = "us-east-1" 
+}
+
+resource "aws_instance" "foo_app_instance" {
+  ami           = "ami-0c55b159cbfafe1f0" 
+  instance_type = "t2.micro"      # Smallest instance for testing
+  tags = {
+    Name = "FooApp-Instance"
+  }
+}
 
